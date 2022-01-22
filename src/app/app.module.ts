6cd584dbfcb2222f7dfb29f47cc8f38f5ffe77df
Lucas Henrique <48369434+lucashfdeus@users.noreply.github.com>
2022-01-22 19:42:49 -0300

    Criando tela de pesquisa

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+
+import { HttpClientModule } from '@angular/common/http'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PessoasComponent } from './pessoas/pessoas.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+      PessoasComponent,
+      NavComponent
+   ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    CollapseModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule,
+    NgbModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
